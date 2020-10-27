3b04b54853a28e75db8d306ca53c8d086b7c2fdc
c0reyes <c_rrl@yahoo.com>
2020-10-27 12:37:06 +0100

    Two unit test.

--- src/test/java/com/dragueo/web/ApplicationTests.java
+++ src/test/java/com/dragueo/web/ApplicationTests.java
@@ -1,13 +1,21 @@
 package com.dragueo.web;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.dragueo.web.controller.DragueoController;
 
 @SpringBootTest
 class ApplicationTests {
+	@Autowired
+	private DragueoController controller;
 
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
